e448bf7cb9f21e0eb2eeb77790e44caf8dc1b998
Nathan Kot <me@nathankot.com>
2019-04-01 23:53:51 +0900

    Update headers

--- lib/ledger_get_prices.rb
+++ lib/ledger_get_prices.rb
@@ -79,7 +79,12 @@ module LedgerGetPrices
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
         req = Net::HTTP::Get.new(uri.request_uri)
-        req.initialize_http_header({'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'})
+        req.initialize_http_header({
+          'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
+          'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+          'Accept-Language' => 'en-US,en;q=0.9',
+          'Cookie' => '',
+        })
         response = http.request(req)
         response_json = JSON.parse response.body
 
